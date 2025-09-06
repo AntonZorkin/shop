c1823b7aa8f3c405236617b62db037390efa3770
AntonZorkin <don_er@ukr.net>
2025-09-06 16:38:09 +0000

    Deploying to gh-pages from @ AntonZorkin/shop@ea889f7a347136ec98502b83996a93b95f9ade7d 🚀

--- wishlist.js
+++ wishlist.js
@@ -1,2 +1,2 @@
-import{c as t,r as e}from"./assets/helpers-BDADh7MJ.js";import"./assets/vendor-N5iQpiFS.js";const s=t("cart");e.cartNumElem.textContent=s.length;const n=t("wishlist");e.wishNumElem.textContent=n.length;
+import{o as e,c as t,r as s}from"./assets/helpers-DhdRKeq6.js";import"./assets/vendor-N5iQpiFS.js";e();const n=t("cart");s.cartNumElem.textContent=n.length;const o=t("wishlist");s.wishNumElem.textContent=o.length;
 //# sourceMappingURL=wishlist.js.map
